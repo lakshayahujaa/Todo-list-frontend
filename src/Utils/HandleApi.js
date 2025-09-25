cca214f556b36216c3e7314a2cf286f4beb7983e
Lakshay Ahuja <121502875+lakshayahujaa@users.noreply.github.com>
2025-09-25 11:54:10 +0530

    Add files via upload

---
+++ src/Utils/HandleApi.js
@@ -0,0 +1,43 @@
+import axios from 'axios'
+
+const baseUrl = "https://todo-list-backend-vdz3.onrender.com"
+
+const getAllToDo = (setToDo) => {
+    axios
+    .get(baseUrl)
+    .then(({data})=> {
+        console.log('data --->', data);
+        setToDo(data)
+    })
+}
+
+const addToDo = (text, setText, setToDo) =>{
+    axios.post(`${baseUrl}/save`, {text})
+    .then((data)=>{
+        console.log(data);
+        setText("")
+        getAllToDo(setToDo)
+    })
+    .catch((err) => console.log(err))
+}
+
+const updateToDo = (toDoID, text, setToDo, setText, setIsUpdating) =>{
+    axios.post(`${baseUrl}/update`, {_id: toDoID, text})
+    .then((data)=>{
+        setText("")
+        setIsUpdating(false)
+        getAllToDo(setToDo)
+    })
+    .catch((err)=> console.log(err))
+}
+
+const deleteToDo = (_id, setToDo) =>{
+    axios.post(`${baseUrl}/delete`, {_id})
+    .then((data)=>{
+        console.log(data)
+        getAllToDo(setToDo)
+    })
+    .catch((err)=> console.log(err))
+}
+
+export{getAllToDo, addToDo, updateToDo, deleteToDo}
